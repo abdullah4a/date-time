7314939818fa6e9a0a13a01cd3b4b65ef71fd7c1
abdullah4a <abdullahkhan4a@gmail.com>
2024-01-30 11:31:25 +0500

    minor fix

--- src/components/DateTime.vue
+++ src/components/DateTime.vue
@@ -167,7 +167,7 @@ const showHint=()=>{
             <button type="button" v-if="timeOnly"
               class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
               @click="() => {
-                timeFormatChange = !timeFormatChange
+                timeFormatChange = timeFormat === "HH:mm:ss"
                 clearPayload()
                 changeTimeFormat()
               }">
